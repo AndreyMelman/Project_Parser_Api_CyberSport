ec4a2c019d27482277ee8b8e504f51fefff0329c
AndreyMelman <lagunmelman@gmail.com>
2024-09-17 02:12:10 +0300

    Changed the parser from syn to asynс

--- app/parser/news.py
+++ app/parser/news.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
@@ -14,46 +14,50 @@ class News:
         }
 
     # Функция парсинга новостного сайта
-    def load_articles(self):
-        # Получаем URL нашего сайта
-        response = requests.get(url=self.url, headers=self.headers)
-        # Получаем HTML-файл
-        soup = BeautifulSoup(response.content, "lxml")
-        # Получаем кусок HTML-файла с которого будем парсить новости
-        articles_items = soup.find_all(
-            "div",
-            class_="rounded-block root_d51Rr with-hover no-padding no-margin",
-        )
+    async def load_articles(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url, headers=self.headers) as response:
+                # Получаем URL нашего сайта
+                response = await session.get(url=self.url, headers=self.headers)
+                # Получаем HTML-файл
+                soup = BeautifulSoup(await response.text(), "lxml")
+                # Получаем кусок HTML-файла с которого будем парсить новости
+                articles_items = soup.find_all(
+                    "div",
+                    class_="rounded-block root_d51Rr with-hover no-padding no-margin",
+                )
 
-        # Создаем словарь для записи новостей
-        news_dict = {}
+                # Создаем словарь для записи новостей
+                news_dict = {}
 
-        for article in articles_items:
-            article_title = article.find("h3", class_="title_nSS03").text
+                for article in articles_items:
+                    article_title = article.find("h3", class_="title_nSS03").text
 
-            article_url = f"https://www.cybersport.ru{article.find('a')['href']}"
+                    article_url = (
+                        f"https://www.cybersport.ru{article.find('a')['href']}"
+                    )
 
-            article_img1 = article.find("img")
-            if article_img1 and "src" in article_img1.attrs:
-                article_img = article_img1["src"]
-            else:
-                article_img = None
+                    article_img1 = article.find("img")
+                    if article_img1 and "src" in article_img1.attrs:
+                        article_img = article_img1["src"]
+                    else:
+                        article_img = None
 
-            date_obj = article.find("time").get("datetime")[:-6]
-            article_date = datetime.strptime(date_obj, "%Y-%m-%dT%H:%M:%S")
-            article_date_time1 = article_date + timedelta(hours=3)
-            article_date_time = article_date_time1.strftime("%Y-%m-%d %H:%M:%S")
+                    date_obj = article.find("time").get("datetime")[:-6]
+                    article_date = datetime.strptime(date_obj, "%Y-%m-%dT%H:%M:%S")
+                    article_date_time1 = article_date + timedelta(hours=3)
+                    article_date_time = article_date_time1.strftime("%Y-%m-%d %H:%M:%S")
 
-            article_category = article.find("a", class_="tag_9QLmg").text
+                    article_category = article.find("a", class_="tag_9QLmg").text
 
-            article_id = str(int(article_date.timestamp()))
+                    article_id = str(int(article_date.timestamp()))
 
-            news_dict[article_id] = {
-                "article_title": article_title,
-                "article_date_time": article_date_time,
-                "article_url": article_url,
-                "article_img": article_img,
-                "article_category": article_category,
-            }
+                    news_dict[article_id] = {
+                        "article_title": article_title,
+                        "article_date_time": article_date_time,
+                        "article_url": article_url,
+                        "article_img": article_img,
+                        "article_category": article_category,
+                    }
 
-        return news_dict
+                return news_dict
