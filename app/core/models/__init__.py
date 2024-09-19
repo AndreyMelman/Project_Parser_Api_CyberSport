0fd13b3929429f508a06c190315b075014e8a188
AndreyMelman <lagunmelman@gmail.com>
2024-09-19 13:09:11 +0300

    ref base

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,7 +1,11 @@
 __all__ = (
     "db_helper",
     "Base",
+    # "User",
+    "News",
 )
 
 from .db_helper import db_helper
 from .base import Base
+# from .users import User
+from .news import News
--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,12 +1,19 @@
-from sqlalchemy.orm import (DeclarativeBase, Mapped, mapped_column, declared_attr,)
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    declared_attr,
+)
+
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
+    # Автоматически генерируемое имя таблицы
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    # id: Mapped[int] = mapped_column(primary_key=True)
