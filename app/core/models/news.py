bba92bba609bb72db734d5c977f53cf77ce198fd
AndreyMelman <lagunmelman@gmail.com>
2024-09-19 13:08:40 +0300

    create table news for save data

---
+++ app/core/models/news.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
+
+
+class News(Base):
+    __tablename__ = "news"
+
+    id_news: Mapped[int] = mapped_column(unique=True)
+    title: Mapped[str]
+    created_at: Mapped[datetime]
+    url: Mapped[str]
+    img_url: Mapped[str]
+    category: Mapped[str]
+    sent_to_telegram: Mapped[bool] = mapped_column(default=False)
